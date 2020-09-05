20abecbfc1b0e2830d59697df93089bbb82c0998
rebeccama <r69ma@uwaterloo.ca>
2020-09-05 13:04:09 -0400

    signup screen

--- react-component/src/Views/LogIn.js
+++ react-component/src/Views/LogIn.js
@@ -9,7 +9,7 @@ function LogIn() {
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Email" />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
