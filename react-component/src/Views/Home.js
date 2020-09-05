61b29c0aaea93ef461ef03df2b47a80bc567dd4b
y2kwak <goodywk07@gmail.com>
2020-09-05 13:13:35 -0400

    modal for uploading

--- react-component/src/Views/Home.js
+++ react-component/src/Views/Home.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 import { Link } from 'react-router-dom'
 
 function Home() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return(
-        <div>
-            This is the Home Page
-            <Button><Link to="/login" >LogIn</Link></Button>
-        </div>
+        
+        <Container className="m-5">
+            <Button variant="primary" href="/login">Log In</Button>{' '}
+            <Button variant="primary" href="/signup">SignUp</Button>{' '}
+            <h1 style={{textAlign: "center", paddingTop: "20%"}}>AppName</h1>
+            <Button variant="primary" onClick={handleShow}>Upload a picture</Button>
+
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Upload your picture</Modal.Title>
+            </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                    <Form.Group>
+                    <Form.File />
+                    </Form.Group>
+                    </Form>
+                </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+            Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+            Save Changes
+            </Button>
+             </Modal.Footer>
+            </Modal>
+        </Container>
     )
 }
 export default Home
